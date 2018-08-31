bc5e75b20fd61e6558fe7414951f2f9fbad220ca
Anderson Albuquerque <andnunes88@gmail.com>
2018-08-31 14:45:35 -0300

    add evento click

--- javascript-projeto-calculadora/scripts/controller/CalcController.js
+++ javascript-projeto-calculadora/scripts/controller/CalcController.js
@@ -8,6 +8,7 @@ class CalcController{
         this._timeEl = document.querySelector("#hora");
         this._currentDate; 
         this.initialize();
+        this.initButtonsEvents();
 
     }
 
@@ -20,6 +21,24 @@ class CalcController{
             this.setDisplayDateTime();
 
         }, 1000);
+    }
+
+    initButtonsEvents(){
+
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+      buttons.forEach((btn, index)=>{
+
+        btn.addEventListener('click', e => {
+
+            console.log(btn.className.baseVal.replace("btn-",""));
+
+        });
+
+      })
+
+        
+
     }
 
     setDisplayDateTime(){
